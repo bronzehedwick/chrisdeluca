343a8f7598f63f053d92b4df86937c4dc925a9da
Chris DeLuca <bronzehedwick@gmail.com>
2017-05-01 22:04:02 -0400

    Minify CSS
    Minifying the css with cssnano to reduce page load time.
    
    Remove monokai.css, since it is not being used.
    
    Add some build requirement documentation to the README.
    
    Closes #65.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Chris DeLuca's Personal Website
 
 This site uses [Hugo](https://gohugo.io) and a custom theme. Feel free to take what you want from it, but please share any code you create under the GPL v3 license terms.
+
+## Requirements
+
+To build the site, the following requirements need to be met.
+
+* Hugo v0.16+
+* Node v4+
+* Unix shell
---
+++ scripts/minify-css.js
@@ -0,0 +1,34 @@
+#!/usr/local/bin/node
+
+const cssnano = require('cssnano');
+const fs = require('fs-extra');
+const path = require('path');
+const dir = './public/css';
+const opts = {
+  discardComments: {
+    removeAll: true
+  }
+};
+
+fs.readdir(dir)
+  .then(processFiles)
+  .catch(processError);
+
+function processFiles(files) {
+  files.forEach(file => {
+    if (path.extname(file) == '.css') {
+      fs.readFile(path.resolve(`${dir}/${file}`), 'utf-8')
+        .then(data => cssnano.process(data, opts))
+        .then(minifyCSS.bind(file))
+        .catch(processError);
+    }
+  });
+}
+
+function minifyCSS(result) {
+  fs.writeFileSync(`${dir}/${this}`, result.css);
+}
+
+function processError(error) {
+  console.error(error.stack);
+}
