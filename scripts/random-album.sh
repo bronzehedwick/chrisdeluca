1ae1da21abfd6d74b671fabce1c7810eee035ff8
Chris DeLuca <bronzehedwick@gmail.com>
2018-02-25 14:03:22 -0500

    Add random album script

---
+++ scripts/random-album.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Generate a random album that I own.
+
+DATA_FILE="$(git rev-parse --show-toplevel)/data/vinyl.csv"
+# Save reference to all abums, minus the first line, which is meta data.
+ALBUMS=$(sed '1d' "$DATA_FILE")
+# Count the total amount of albums for future use.
+TOTAL_ALBUMS=$(echo "$ALBUMS" | wc -l)
+# Generate a random number between 1 and the total number of albums.
+RANDOM_NUM=$(( 1 + RANDOM % TOTAL_ALBUMS ))
+# Increment the random number for sed's syntax.
+INCREMENTED_NUM=$(( 1 + RANDOM_NUM ))
+# Display the album generated in pretty format (via column).
+echo "$ALBUMS" | cut -d, -f2,3 | column -t -s ',' | sed -n "$RANDOM_NUM,$RANDOM_NUM p;$INCREMENTED_NUM q"
