38a4a6640df3c15d5d383b4298ba2ad6a0dadc67
Chris DeLuca <iam@chrisdeluca.me>
2019-08-17 12:36:23 -0400

    Move goat counter to external file
    Give up on an inline script working with CSP.

---
+++ themes/novel/assets/scripts/vendor/goat-counter.js
@@ -0,0 +1,13 @@
+(function() {
+  if (window.location.hostname.indexOf('localhost') > -1) {
+    return;
+  }
+
+  var script = document.createElement('script');
+  window.counter = 'https://chrisdeluca.goatcounter.com/count'
+  script.async = 1;
+  script.src = '//static.goatcounter.com/count.min.js';
+
+  var ins = document.getElementsByTagName('script')[0];
+  ins.parentNode.insertBefore(script, ins)
+})();
