84e2fd9ad93e7922886524d1339fda62aeb7e9de
Chris DeLuca <code@chrisdeluca.me>
2020-11-16 08:42:06 -0500

    Remove webmentions
    Interesting feature, but too complex to use, and it only ever worked for me a few times. Too much overhead for too little value.

--- themes/novel/assets/scripts/webmentions.js
+++
@@ -1,8 +0,0 @@
-(function webmentions() {
-    'use strict';
-
-    fetch(`https://webmention.io/api/mentions.jf2?target=${window.location}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error));
-})();
